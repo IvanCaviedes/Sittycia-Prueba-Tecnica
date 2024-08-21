e57c5832d378793defae9387966af4f06def7b97
Ivan Caviedes <ivancaviedes99@gmail.com>
2024-08-21 08:39:52 -0500

    creando Context EntityFramework

---
+++ Backend/Backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+
+namespace Backend.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the one-to-many relationship
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Tasks)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId);
+
+
+        }
+    }
+}
