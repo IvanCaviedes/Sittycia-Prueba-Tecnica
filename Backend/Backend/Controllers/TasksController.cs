e08210bdb4252010ca4d4d8b08b76b48cd2edfc3
Ivan Caviedes <ivancaviedes99@gmail.com>
2024-08-21 08:40:46 -0500

    Creando Controlador Task

---
+++ Backend/Backend/Controllers/TasksController.cs
@@ -0,0 +1,108 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TasksController(ApplicationDbContext context, ILogger<WeatherForecastController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Task>>> GetTasks()
+        {
+            var tasks = _context.Tasks.Include(t => t.User);
+            return await tasks.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.Task>> GetTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, Models.Task task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Models.Task>> PostTask(Models.Task task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTask", new { id = task.Id }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
